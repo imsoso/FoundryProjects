d39a0df5269cee56a719eaaf83044d9cfe17f8ba
Soso <husosong@163.com>
2024-10-16 20:35:48 +0800

    Feature: Implement list function to put token on the market

--- src/NFTMarket.sol
+++ src/NFTMarket.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
-/*用 ERC721 标准（可复用 OpenZepplin 库）发行一个自己 NFT 合约，并用图片铸造几个 NFT ， 请把图片和 Meta Json数据上传到去中心的存储服务中，请贴出在 OpenSea 的 NFT 链接。
-
-发⾏⼀个 ERC721 Token（⽤⾃⼰的名字）
-• 铸造⼏个 NFT，在测试⽹上发⾏，在 Opensea 上查看
-• 编写⼀个市场合约 NFTMarket：使⽤⾃⼰发⾏的 ERC20 Token   来买 NFT：
-• NFT 持有者可上架 NFT（list() 设置价格 多少个 TOKEN 购买 NFT  ）
-• 编写购买NFT ⽅法 buyNFT(uint tokenID, uint amount)，转⼊对应的TOKEN，获取对应的  NFT
-
- 熟悉 ERC721 实现
-• https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/
- ERC721/ERC721.sol
- • safeTransferFrom 与合约如何接收 NFT 
-• 实现 onERC721Received(
+/*
+编写一个简单的 NFTMarket 合约，使用自己发行的ERC20 扩展 Token 来买卖 NFT， NFTMarket 的函数有：
+list() : 实现上架功能，NFT 持有者可以设定一个价格（需要多少个 Token 购买该 NFT）并上架 NFT 到 NFTMarket，上架之后，其他人才可以购买。
+buyNFT(uint tokenID, uint amount) : 普通的购买 NFT 功能，用户转入所定价的 token 数量，获得对应的 NFT。
+实现ERC20 扩展 Token 所要求的接收者方法 tokensReceived  ，在 tokensReceived 中实现NFT 购买功能。
  */
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity >=0.8.20;
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+contract NFTMarket is IERC721Receiver {
+    // NFT contract
+    IERC721 public nftMarket;
+    constructor(IERC721 _nftContract) {
+        nftMarket = _nftContract;
+    }
 
-contract NFTMarket is ERC20 {
-    constructor() public ERC20("NFTMarket", "NFTM") {}
+    // List NFT on the market
+    function list(uint256 tokenId, uint256 price) external {
+        // Transfer NFT to the market, make it available for sale
+        nftMarket.safeTransferFrom(msg.sender, address(this), tokenId);
+    }
 }
