dc868ec1bf685007bffc693c46f9dd5f52e3b6c8
Soso <husosong@163.com>
2024-10-24 14:31:53 +0800

    Feature: setup test for token and bank

--- solidity-scripting/src/day12/test/TokenBankTest.sol
+++ solidity-scripting/src/day12/test/TokenBankTest.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {TokenBank} from "../TokenBank.sol";
+import "../SosoToken2621.sol";
 
-contract BankTest is Test {}
+contract BankTest is Test {
+    TokenBank public bank;
+    SosoToken2621 public token;
+
+    function setUp() public {
+        token = new SosoToken2621("SosoToken2612", "STK");
+        bank = new TokenBank(address(token));
+    }
+}
