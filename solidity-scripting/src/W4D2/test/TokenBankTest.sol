99d765e8a33ac1eda51cf6367299f106580bf284
Soso <husosong@163.com>
2024-11-03 17:57:15 +0800

    Feature:Init test

---
+++ solidity-scripting/src/W4D2/test/TokenBankTest.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MyIDO} from "../MyIDO.sol";
+
+contract MyIDOTest is Test {
+
+    function setUp() public {
+
+    }
+}
