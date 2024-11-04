a1494690e82f8180c01ff3d98246af7b030bbda0
Soso <husosong@163.com>
2024-11-04 11:49:31 +0800

    Improvement: Rename test file

---
+++ solidity-scripting/src/W4D2/test/MyIDOTest.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MyIDO} from "../MyIDO.sol";
+import {MyToken} from "../../MyToken.sol";
+
+
+contract MyIDOTest is Test {
+    MyIDO public myIDO;
+    MyToken public token;
+
+    address public ownerAccount;
+    address public contributorAlice;
+    address public contributorBob;
+
+
+    function setUp() public {
+        token = new MyToken("MyToken", "TKN");
+        myIDO = new MyIDO(token, 0.0001 ether, 100 ether, 200 ether, 1 weeks, 1000000);
+
+        contributorAlice = makeAddr("Alice");
+        contributorBob = makeAddr("Bob");
+    }
+
+
+    function testContributeSucceed() public {
+        vm.deal(contributorAlice, 1 ether);
+        vm.prank(contributorAlice);
+        (bool callSuccess, ) = contributorAlice.call{value: 0.0001 ether}(
+        abi.encodeWithSignature("contribute()"));
+        require(callSuccess, "Call failed");
+
+       uint256 IDOBalance = myIDO.currentTotalFunding();
+       uint256 aliceContribution = myIDO.balances(contributorAlice);
+
+        assertEq(IDOBalance, aliceContribution, "Incorrect funding amount");
+    }
+}
