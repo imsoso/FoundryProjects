ff7dc3b0dce3be85da81ad9a60b4e64a66355e21
Soso <husosong@163.com>
2024-11-04 20:07:51 +0800

    Feature:Init Assignment for Staking 编写一个质押挖矿合约，实现如下功能：
    1、用户随时可以质押项目方代币 RNT(自定义的ERC20) ，开始赚取项目方Token(esRNT)；
    2、可随时解押提取已质押的 RNT；
    3、可随时领取esRNT奖励，每质押1个RNT每天可奖励 1 esRNT;
    4、esRNT 是锁仓性的 RNT， 1 esRNT 在 30 天后可兑换 1 RNT，随时间线性释放，支持提前将 esRNT 兑换成 RNT，但锁定部分将被 burn 燃烧掉。

---
+++ solidity-scripting/src/W4D2/erRNT.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+/*
+编写一个质押挖矿合约，实现如下功能：
+
+1、用户随时可以质押项目方代币 RNT(自定义的ERC20) ，开始赚取项目方Token(esRNT)；
+2、可随时解押提取已质押的 RNT；
+3、可随时领取esRNT奖励，每质押1个RNT每天可奖励 1 esRNT;
+4、esRNT 是锁仓性的 RNT， 1 esRNT 在 30 天后可兑换 1 RNT，随时间线性释放，支持提前将 esRNT 兑换成 RNT，但锁定部分将被 burn 燃烧掉。
+*/
+pragma solidity >=0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract esRNT is ERC20 {
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, 1e10 * 1e18);
+    }
+}
