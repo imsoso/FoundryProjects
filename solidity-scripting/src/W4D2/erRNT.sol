ac1351fd0d9adf5bb62f0da2b3c601bafa692832
Soso <husosong@163.com>
2024-11-04 20:42:00 +0800

    Feature: Make esRNT mint and burnable

--- solidity-scripting/src/W4D2/erRNT.sol
+++ solidity-scripting/src/W4D2/erRNT.sol
@@ -9,13 +9,18 @@
 */
 pragma solidity >=0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract esRNT is ERC20 {
+contract esRNT is ERC20Burnable {
     constructor(
         string memory name_,
         string memory symbol_
     ) ERC20(name_, symbol_) {
         _mint(msg.sender, 1e10 * 1e18);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
     }
 }
