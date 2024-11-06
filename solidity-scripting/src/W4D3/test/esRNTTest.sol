50bbc6beef56125458293f8e0bd8c745c8442062
Soso <husosong@163.com>
2024-11-06 18:06:46 +0800

    Improvement: Add Contract to read

--- solidity-scripting/src/W4D3/test/esRNTTest.sol
+++ solidity-scripting/src/W4D3/test/esRNTTest.sol
@@ -5,3 +5,20 @@ import {Test, console} from "forge-std/Test.sol";
 import {esRNT} from "../esRNT.sol";
 
 contract MyESRNTTest is Test {}
+/* 
+// Contract to read
+contract esRNT {
+    struct LockInfo {
+        address user; // 20 bytes
+        uint64 startTime; // 8 bytes
+        uint256 amount;// 32 bytes
+    }
+    LockInfo[] private _locks;
+
+    constructor() {
+        for (uint256 i = 0; i < 11; i++) {
+        _locks.push(LockInfo(address(uint160(i+1)), uint64(block.timestamp * 2 - i), 1e18 * (i + 1)));
+        }
+    }
+}
+*/
