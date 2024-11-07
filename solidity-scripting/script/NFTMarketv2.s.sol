e335abf8fb78bef730fdf78230d72df2de7f2b9e
Soso <husosong@163.com>
2024-11-07 20:48:46 +0800

    Feature: Deploy v1 and upgrade v2

--- solidity-scripting/script/NFTMarketv2.s.sol
+++ solidity-scripting/script/NFTMarketv2.s.sol
@@ -1,21 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
-import "../src/W4D4/NFTMarketV2.sol";
+import {Script, console} from "forge-std/Script.sol";
+import "../src/W4D4/NFTMarketV1.sol";
+import {NFTMarketV2} from "../src/W4D4/NFTMarketV2.sol";;
+
 import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract NFTMarketScript is Script {
-    function run() public broadcaster {
+    function run() public {
         Options memory opts;
         opts.unsafeSkipAllChecks = true;
-        opts.referenceContract = "Counter.sol";
-        // proxy: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
-        Upgrades.upgradeProxy(
-            0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512,
-            "NFTMarketv2.sol",
+        opts.referenceContract = "NFTMarketV1.sol";
+
+        address deployer = vm.addr(vm.envUint("PRIVATE_KEY"));
+        address aNFTMarketV1 = address(new NFTMarketV1());
+
+        address proxyAddress = Upgrades.deployTransparentProxy(
+            "NFTMarketV1.sol",
+            deployer,
             "",
             opts
+        );
+
+        address NFTMarketV1Addr = Upgrades.getImplementationAddress(
+            proxyAddress
+        );
+        Upgrades.upgradeProxy(proxyAddress, "NFTMarketV2.sol", abi.encodeCall(NFTMarketV2.initialize,(deployer)), opts);
+        address NFTMarketV2Addr = Upgrades.getImplementationAddress(
+            proxyAddress
+        );
+
+        vm.stopBroadcast();
+
+        console.log("deployer:", deployer);
+        console.log("proxy addr:", proxyAddress);
+        console.log("v1 addr:", aNFTMarketV1);
+        console.log(
+            "v2 addr:",
+            Upgrades.getImplementationAddress(proxyAddress)
         );
     }
 }
