a415f8000caa7b1c54e73665bb8e99d38040fe98
Soso <husosong@163.com>
2024-11-07 22:07:59 +0800

    Bugfix: Fix delpoy errors

--- solidity-scripting/script/NFTMarketv2.s.sol
+++ solidity-scripting/script/NFTMarketv2.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 import "../src/W4D4/NFTMarketV1.sol";
-import {NFTMarketV2} from "../src/W4D4/NFTMarketV2.sol";;
+import {NFTMarketV2} from "../src/W4D4/NFTMarketV2.sol";
 
 import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
@@ -13,20 +13,26 @@ contract NFTMarketScript is Script {
         opts.unsafeSkipAllChecks = true;
         opts.referenceContract = "NFTMarketV1.sol";
 
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         address deployer = vm.addr(vm.envUint("PRIVATE_KEY"));
-        address aNFTMarketV1 = address(new NFTMarketV1());
 
+        bytes memory initData = new bytes(0);
         address proxyAddress = Upgrades.deployTransparentProxy(
             "NFTMarketV1.sol",
             deployer,
-            "",
+            initData,
             opts
         );
 
         address NFTMarketV1Addr = Upgrades.getImplementationAddress(
             proxyAddress
         );
-        Upgrades.upgradeProxy(proxyAddress, "NFTMarketV2.sol", abi.encodeCall(NFTMarketV2.initialize,(deployer)), opts);
+        Upgrades.upgradeProxy(
+            proxyAddress,
+            "NFTMarketV2.sol",
+            abi.encodeCall(NFTMarketV2.initialize, (deployer)),
+            opts
+        );
         address NFTMarketV2Addr = Upgrades.getImplementationAddress(
             proxyAddress
         );
@@ -35,10 +41,7 @@ contract NFTMarketScript is Script {
 
         console.log("deployer:", deployer);
         console.log("proxy addr:", proxyAddress);
-        console.log("v1 addr:", aNFTMarketV1);
-        console.log(
-            "v2 addr:",
-            Upgrades.getImplementationAddress(proxyAddress)
-        );
+        console.log("v1 addr:", NFTMarketV1Addr);
+        console.log("v2 addr:", NFTMarketV2Addr);
     }
 }
