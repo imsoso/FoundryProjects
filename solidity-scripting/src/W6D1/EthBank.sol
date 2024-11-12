e8482288ce355d544ec124f94b656f4fd80ee486
Soso <husosong@163.com>
2024-11-12 20:52:08 +0800

    Bugfix: it is a bank accept eth, not token

--- solidity-scripting/src/W6D1/EthBank.sol
+++ solidity-scripting/src/W6D1/EthBank.sol
@@ -1,22 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-/*
-使用 EIP2612 标准（可基于 Openzepplin 库）编写一个自己名称的 Token 合约。
-修改 TokenBank 存款合约 ,添加一个函数 permitDeposit 以支持离线签名授权（permit）进行存款。
-修改Token 购买 NFT NTFMarket 合约，添加功能 permitBuy() 实现只有离线授权的白名单地址才可以购买 NFT （用自己的名称发行 NFT，再上架） 。白名单具体实现逻辑为：项目方给白名单地址签名，白名单用户拿到签名信息后，传给 permitBuy() 函数，在permitBuy()中判断时候是经过许可的白名单用户，如果是，才可以进行后续购买，否则 revert 。
-
-要求：
-有 Token 存款及 NFT 购买成功的测试用例
-有测试用例运行日志或截图，能够看到 Token 及 NFT 转移。
-*/
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import './SosoToken2621.sol';
 import { AutomationCompatibleInterface } from './AutomationCompatibleInterface.sol';
 
-contract TokenBank is AutomationCompatibleInterface {
+contract ETHBank is AutomationCompatibleInterface {
     address admin;
-    IERC20 public token;
-    SosoToken2621 public tokenPermit;
 
     mapping(address => uint) internal balances;
 
@@ -24,10 +12,8 @@ contract TokenBank is AutomationCompatibleInterface {
     uint256 public immutable interval;
     uint256 public lastTimeStamp;
 
-    constructor(address _token, uint256 refreshInterval) {
-        admin = msg.sender;
-        token = IERC20(_token);
-        tokenPermit = SosoToken2621(_token);
+    constructor(address _admin, uint256 refreshInterval) {
+        admin = _admin;
 
         interval = refreshInterval;
         lastTimeStamp = block.timestamp;
@@ -76,7 +62,7 @@ contract TokenBank is AutomationCompatibleInterface {
     function checkUpkeep(bytes calldata checkData) external view override returns (bool upkeepNeeded, bytes memory performData) {
         bool timePassed = (block.timestamp - lastTimeStamp) > interval;
 
-        uint256 threshold = 1 ether;
+        uint256 threshold = 0.0001 ether;
         bool balanceReachThreshold = address(this).balance > threshold;
         upkeepNeeded = timePassed && balanceReachThreshold;
     }
