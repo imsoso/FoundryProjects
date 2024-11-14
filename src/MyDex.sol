1b1e2078259954b955f03bea82acc7fdea7c5cd8
Soso <husosong@163.com>
2024-11-14 17:49:46 +0800

    Feature :Init MyDex ➤ 编写 MyDex 合约，任何人都可通过 MyDex 来买卖ETH ➤ 任何人都可以通过 sellETH 方法出售ETH兑换成 USDT，也可以通过 buyETH 将 USDT 兑换成 ETH。

---
+++ src/MyDex.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+/*
+➤ 部署自己的 UniswapV2 Dex
+➤ 编写 MyDex 合约，任何人都可通过 MyDex 来买卖ETH  
+➤ 任何人都可以通过 sellETH 方法出售ETH兑换成 USDT，也可以通过 buyETH 将 USDT 兑换成 ETH。
+➤ Test合约测试：创建RNT-ETH交易对、添加初始化流动性、移除流动性、使用 RNT兑换 ETH，用 ETH兑换RNT
+*/
+
+contract MyDex {}
