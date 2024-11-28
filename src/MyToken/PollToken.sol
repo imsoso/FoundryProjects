e603c336be5c79291a7e1bf29c500a006e76da69
Soso <husosong@163.com>
2024-11-28 20:28:45 +0800

    Delete useless overrides

--- src/MyToken/PollToken.sol
+++ src/MyToken/PollToken.sol
@@ -3,39 +3,23 @@
 pragma solidity ^0.8.20;
 
 import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import { ERC20Pausable } from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol';
 import { ERC20Permit } from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol';
 import { ERC20Votes } from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol';
 import { Nonces } from '@openzeppelin/contracts/utils/Nonces.sol';
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 
-contract PollToken is ERC20, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes {
-    constructor(address initialOwner) ERC20('PollToken', 'PTK') Ownable(initialOwner) ERC20Permit('PollToken') {}
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
+contract PollToken is Ownable, ERC20Permit, ERC20Votes {
+    constructor(address initialOwner) ERC20('PollToken', 'PTK') Ownable(initialOwner) ERC20Permit('PollToken') {
+        _mint(initialOwner, 1_000_000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function clock() public view override returns (uint48) {
-        return uint48(block.timestamp);
-    }
-
-    // solhint-disable-next-line func-name-mixedcase
-    function CLOCK_MODE() public pure override returns (string memory) {
-        return 'mode=timestamp';
-    }
-
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable, ERC20Votes) {
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
         super._update(from, to, value);
     }
 
